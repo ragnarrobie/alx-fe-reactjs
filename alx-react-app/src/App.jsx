048d0eda857ceeaa8c8a476d3287b51acfba801f
Robson Taye <ragnarrobie@gmail.com>
2025-02-17 13:35:00 +0300

    new b

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,44 +7,12 @@ import WelcomeMessage from './WelcomeMessage';
 import MainContent from './MainContent'
 import Footer from './Footer'
 import UserProfile from './UserProfile';
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        
-        <WelcomeMessage />
-        <Header />
-        <MainContent />
-        <Footer />
-        
-        
-      </div>
-      <h1>vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <div>
-          
-
-        </div>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const App = () => {
+  return(
+<div>
+  <h1>Welcome to the User Profile App</h1>
+  <UserProfile name="Alice"age={25} bio="Loves hiking and photography" />
+</div>
   )
 }
-
 export default App;
