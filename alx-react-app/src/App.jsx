c9ea6cae37088cc4f42d365e36d8bad4e1ac490b
Robson Taye <ragnarrobie@gmail.com>
2025-02-17 13:39:53 +0300

    new c

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return(
 <div>
   <h1>Welcome to the User Profile App</h1>
-  <UserProfile name="Alice"age={25} bio="Loves hiking and photography" />
+  <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
 </div>
   )
 }
--- alx-react-app/src/UserProfile.jsx
+++ alx-react-app/src/UserProfile.jsx
@@ -3,8 +3,8 @@ const UserProfile =(props)=>{
     return (
         <div>
         <h2>{props.name}</h2>
-        <p>Age:{props.Age}</p>
-        <p>Bio:{props.Bio}</p>
+        <p>Age:{props.age}</p>
+        <p>Bio:{props.bio}</p>
         </div>
     )
 }
